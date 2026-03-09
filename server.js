241386caaa7a9b9a48f842e11d67e89c3e8a9694
laura8nora <lauranora.gyulai@skola.taby.se>
2026-03-09 19:12:32 +0100

    Update server.js

--- server.js
+++ server.js
@@ -18,3 +18,19 @@ db.connect(err => {
   }
   console.log("Connected to database");
 });
+
+app.get("/", (req, res) => {
+  res.send(`
+    <h1>My API</h1>
+    <p>Available routes:</p>
+    <ul>
+      <li>GET /users - Get all users</li>
+      <li>GET /users/:id - Get a single user by ID</li>
+      <li>POST /users - Create a new user (send JSON {"name": "Anna"})</li>
+    </ul>
+  `);
+});
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
